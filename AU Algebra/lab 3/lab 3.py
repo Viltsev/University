2c3a2dcf425283841d1ebecbfb0a55a69cb07f4c
Viltsev <94001621+Viltsev@users.noreply.github.com>
2023-04-04 15:37:00 +0400

    au algebra commit
    lab 3. add point 2

--- AU Algebra/lab 3/lab 3.py
+++ AU Algebra/lab 3/lab 3.py
@@ -31,30 +31,108 @@ def check_assocoativity(cayleyTable):
                     return "Операция неассоциативна"
     return "Операция ассоциативна"
 
+# функция получения подполугруппы
+"""
+подмножество: {2}
+таблица:
+0, 1, 2, 3, 4, 5, 6, 7
+1, 2, 3, 4, 5, 6, 7, 0
+2, 3, 4, 5, 6, 7, 0, 1
+3, 4, 5, 6, 7, 0, 1, 2
+4, 5, 6, 7, 0, 1, 2, 3
+5, 6, 7, 0, 1, 2, 3, 4
+6, 7, 0, 1, 2, 3, 4, 5
+7, 0, 1, 2, 3, 4, 5, 6
+
+полугруппа: 0, 1, 2, 3, 4, 5, 6, 7
+
+1. берем элемент из текущего подмножества и кладем его в подполугруппу
+2. далее находим индекс данного элемента в полугруппе
+3. проделываем шаг 2 для остальных элементов в подполугруппе
+4. i-ую позицию умножаем на j-ую и полученный элемент кладем в подполугруппу
+
+"""
+def findSubsemigroup(cayleTable, X, semigroup):
+    result = []
+    for i in X:
+        result.append(i)
+    for i in result:
+        index1 = semigroup.index(i)
+        for j in result:
+            index2 = semigroup.index(j)
+            elementToAppend = cayleTable[index1][index2]
+            if not(elementToAppend in result):
+                result.append(elementToAppend)
+    result = set(result)
+    return result
+    
+
+
+
 
 """
 MAIN PROGRAM
 """
 print("Лабораторная работа №3")
 
-# 1. Алгоритм теста Лайта проверки ассоциативности бинарной операции
-print("1. Алгоритм теста Лайта проверки ассоциативности бинарной операции")
-n = int(input("Введите размер таблицы Кэли"))
-matrix = []
+"""
+1. Алгоритм теста Лайта проверки ассоциативности бинарной операции
+"""
 
-print("Введите таблицу Кэли: ")
-matrix = [list(map(int, input(f"Enter row {i+1}: ").split())) for i in range(n)]
+# print("1. Алгоритм теста Лайта проверки ассоциативности бинарной операции")
+# n = int(input("Введите размер таблицы Кэли"))
+# cayleyTable = []
+#
+# print("Введите таблицу Кэли: ")
+# cayleyTable = [list(map(int, input(f"Enter row {i+1}: ").split())) for i in range(n)]
+#
+# # вывод таблицы Кэли
+# print("Таблица Кэли: ")
+# for i in range(n):
+#     for j in range(n):
+#         print(cayleyTable[i][j], end=" ")
+#     print()
+#
+# # проверка на ассоциативность
+# assocoativityCheck = check_assocoativity(cayleyTable)
+# print(assocoativityCheck)
 
 """
-ВЫВОД ТАБЛИЦЫ КЭЛИ
+2. Алгоритм построения подполугрупп по таблице Кэли.
 """
+print("2. Алгоритм построения подполугрупп по таблице Кэли.")
+# число элементов в полугруппе
+# countS = int(input("Введите число элементов в полугруппе S: "))
+
+# элементы полугруппы
+semigroupElements = [0, 1, 2, 3, 4, 5, 6, 7]
+# print("Введите элементы полугруппы: ")
+# for i in range(countS):
+#     semigroupElements.append(int(input()))
+
+# таблица Кэли
+# cayleyTable2 = []
+# print("Введите таблицу Кэли: ")
+# cayleyTable2 = [list(map(int, input(f"Enter row {i+1}: ").split())) for i in range(countS)]
+cayleyTable2 = [[0, 1, 2, 3, 4, 5, 6, 7],
+                 [1, 2, 3, 4, 5, 6, 7, 0],
+                 [2, 3, 4, 5, 6, 7, 0, 1],
+                 [3, 4, 5, 6, 7, 0, 1, 2],
+                 [4, 5, 6, 7, 0, 1, 2, 3],
+                 [5, 6, 7, 0, 1, 2, 3, 4],
+                 [6, 7, 0, 1, 2, 3, 4, 5],
+                 [7, 0, 1, 2, 3, 4, 5, 6]]
+# число элементов в подмножестве
+# countX = int(input("Введите число элементов в подмножестве X: "))
+countX = 1
+# элементы подмножества
+subset = [2]
+# print("Введите элементы подмножества: ")
+# for i in range(countX):
+#     subset.append(int(input()))
+
+result = findSubsemigroup(cayleyTable2, subset, semigroupElements)
 print("Таблица Кэли: ")
-for i in range(n):
-    for j in range(n):
-        print(matrix[i][j], end=" ")
-    print()
-
-assocoativityCheck = check_assocoativity(matrix)
-print(assocoativityCheck)
-
-
+for i in cayleyTable2:
+    print(i)
+print("Подполугруппа: ", result)
